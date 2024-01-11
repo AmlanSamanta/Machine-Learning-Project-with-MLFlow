09e3758e15bed4812a35876c5bc37c43ad0552d1
Amlan Samanta <xdf.vtt@gmail.com>
2024-01-11 11:19:35 +0530

    data ingestion component changes added

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 from mlopsProject import logger
+from mlopsProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+# logger.info("Welcome to the world of custom logging!")
 
-logger.info("Welcome to the world of custom logging!")
+STAGE_NAME = "Data Ingestion Stage"
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> {STAGE_NAME} started >>>>>>>>")
+        data_ingestion = DataIngestionTrainingPipeline()
+        data_ingestion.main()
+        logger.info(f">>>>>> {STAGE_NAME} completed >>>>>>")
+    except Exception as e:
+        logger.exception(e)
+        raise e
