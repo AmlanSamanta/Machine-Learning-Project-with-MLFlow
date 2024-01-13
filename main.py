7ef6c4707b626acc70d4b8c1bbea3487d7750911
Amlan Samanta <xdf.vtt@gmail.com>
2024-01-14 00:30:47 +0530

    model trainer added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from mlopsProject import logger
 from mlopsProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from mlopsProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from mlopsProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from mlopsProject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 # logger.info("Welcome to the world of custom logging!")
 
 # STAGE_NAME = "Data Ingestion Stage"
@@ -42,6 +43,16 @@ try:
    logger.info(f">>>>>> {STAGE_NAME} started <<<<<<") 
    data_transformation = DataTransformationTrainingPipeline()
    data_transformation.main()
+   logger.info(f">>>>>> {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Model Trainer stage"
+try:
+   logger.info(f">>>>>> {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelTrainerTrainingPipeline()
+   data_ingestion.main()
    logger.info(f">>>>>> {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
